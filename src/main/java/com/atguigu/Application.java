673354614cc85d9bf6e6d0b2a10e972a94ad2559
王嘉棣 <478345933@qq.com>
2020-09-27 20:55:41 +0800

    start

--- src/main/java/com/atguigu/Application.java
+++ src/main/java/com/atguigu/Application.java
@@ -2,10 +2,11 @@ package com.atguigu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
+@SpringBootApplication//声明当前项目是一个springboot工程
 public class Application {
 
     public static void main(String[] args) {
+        //启动服务器，springboot项目内置tomcat
         SpringApplication.run(Application.class,args);
     }
 
--- src/main/java/com/atguigu/controller/HelloController.java
+++ src/main/java/com/atguigu/controller/HelloController.java
@@ -1,12 +1,18 @@
 package com.atguigu.controller;
 
+import com.atguigu.properties.DataSourceProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController//组合注解，相当于@Controller + @ResponseBody
 public class HelloController {
+    @Autowired
+    DataSourceProperties dataSourceProperties;
     @RequestMapping("/hello")
     public String sayHello(){
+        System.out.println(dataSourceProperties);
         return "hello world";
     }
 }
